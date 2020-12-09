1b2ad070bd1dc7eb0b147d367de9790de1ccd529
Amina KACIMI <amina.kacimi@etudiant.univ-lr.fr>
2020-12-09 22:05:48 +0100

    initial commit

---
+++ analyste/src/main/java/com/example/analyste/AnalysteApplication.java
@@ -0,0 +1,13 @@
+package com.example.analyste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalysteApplication.class, args);
+    }
+
+}
